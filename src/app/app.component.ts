4ec9d87aba9b0f13623d42d23eb062809a13222b
bandarkarsharad <39089703+pd9538@users.noreply.github.com>
2024-02-24 14:45:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { faArrowRight, faArrowRightLong, faClose, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend_practice';
+  faSearch=faSearch;
+  faClose=faClose;
+  faarrowright=faArrowRightLong;
+  brandlogo:any;
+  showinputbox:boolean=false;
+
+  constructor(private renderer:Renderer2){}
+
+  hideElement(id:number){
+    this.brandlogo=document.getElementsByClassName('navbar-brand');
+    if(id==1){
+     for(let i=0;i<this.brandlogo.length;i++){
+      const element=this.brandlogo[i];
+      this.renderer.addClass(element,'d-none');
+     }
+      this.showinputbox=true;
+    }else if(id==2){
+      this.showinputbox=false;
+      for(let i=0;i<this.brandlogo.length;i++){
+        const element=this.brandlogo[i];
+        this.renderer.removeClass(element,'d-none');
+       }
+    }
+    
+  }
 }
